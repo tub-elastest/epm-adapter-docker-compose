ad09ed04c284ce7bc3b4f750f86e6cce6c433562
radoslavvlaskovski <radoslav.vlaskovsky@gmail.com>
2019-02-05 11:05:42 +0100

    register using keystone token

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:2.7
 
 RUN apt-get update && apt-get install -y build-essential autoconf libtool 
 
-RUN pip install docker-compose==1.20.0 grpcio grpcio-tools pyyaml
+RUN pip install docker-compose==1.20.0 grpcio grpcio-tools pyyaml keystoneauth1 python-keystoneclient
 
 RUN curl -fsSLO https://get.docker.com/builds/Linux/x86_64/docker-17.04.0-ce.tgz \
   && tar xzvf docker-17.04.0-ce.tgz \
--- src/compose_adapter/utils/epm_utils.py
+++ src/compose_adapter/utils/epm_utils.py
@@ -5,6 +5,12 @@ import time
 import requests
 import json
 import logging
+import os
+import sys
+
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
+from keystoneclient.v3 import client
 
 max_timeout = 10
 
@@ -20,9 +26,11 @@ def register_adapter(ip, compose_ip):
     while i < 10:
         pop_compose = {"name": "compose-" + compose_ip,
                        "interfaceEndpoint": compose_ip,
-                       "interfaceInfo":[{"key": "type","value": "docker-compose"}]}
+                       "interfaceInfo": [{"key": "type", "value": "docker-compose"}]}
 
-        headers = {"accept": "application/json","content-type": "application/json"}
+        headers = {"accept": "application/json", "content-type": "application/json"}
+        headers = add_keystone_headers(headers)
+
         try:
             logging.info("Trying to register to EPM at: " + ip)
             identifier = stub.RegisterAdapter(adapter)
@@ -47,5 +55,36 @@ def unregister_adapter(ip, adapter_id, pop_id):
     stub.DeleteAdapter(identifier)
 
     headers = {"accept": "application/json", "content-type": "application/json"}
+    headers = add_keystone_headers(headers)
     r = requests.delete('http://' + ip + ':8180/v1/pop/' + pop_id, headers=headers)
     logging.debug("Deleting pop resulted in: " + str(r.status_code) + " " + r.reason)
+
+
+def add_keystone_headers(headers):
+    if is_keystone_enabled():
+        logging.debug("Keystone enabled!")
+        logging.debug(get_keystone_credentials())
+        keystone_endpoint, keystone_port, keystone_version, keystone_username, keystone_password, keystone_domain = get_keystone_credentials()
+        auth = v3.Password(auth_url=keystone_endpoint + ":" + keystone_port + "/" + keystone_version,
+                           username=keystone_username, password=keystone_password,
+                           project_name=keystone_username,
+                           user_domain_name=keystone_domain, project_domain_name=keystone_domain)
+        sess = session.Session(auth=auth)
+        keystone = client.Client(session=sess)
+        try:
+            keystone.authenticate(auth_url=keystone_endpoint + ":" + keystone_port + "/" + keystone_version,
+                              username=keystone_username, password=keystone_password,
+                              project_name=keystone_username,
+                              user_domain_name=keystone_domain, project_domain_name=keystone_domain)
+            headers["Authorization"] = keystone.auth_ref.auth_token
+        except:
+            logging.error("Unexpected error: " + str(sys.exc_info()))
+    return headers
+
+def is_keystone_enabled():
+    return os.environ.get('KEYSTONE_ENABLED', 'false') == "true"
+
+
+def get_keystone_credentials():
+    return os.environ.get('KEYSTONE_ENDPOINT', ''), os.environ.get('KEYSTONE_PORT', ''), os.environ.get('KEYSTONE_VERSION', ''),\
+           os.environ.get('KEYSTONE_USERNAME', ''), os.environ.get('KEYSTONE_PASSWORD', ''), os.environ.get('KEYSTONE_DOMAIN', '')
